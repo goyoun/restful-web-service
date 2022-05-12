cffa8e5d8c8ecfde28a58a72dfb20a231015cccc
goyoun <92144728+goyoun@users.noreply.github.com>
2022-05-12 11:30:04 +0900

    up

--- src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SecurityConfig.java
@@ -3,10 +3,22 @@ package com.example.restfulwebservice.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+//      h2-console 뒤에 어떤 메소드로 전달되어도 통과될수있게
+        http.authorizeRequests().antMatchers("/h2-console/**").permitAll();
+//      csrf에 관련된거 사용안한다는
+        http.csrf().disable();
+//      헤더의 프레임값을 사용 안한다.
+        http.headers().frameOptions().disable();
+    }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth)
         throws Exception {
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -9,6 +9,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -18,7 +22,10 @@ import java.util.Date;
 //@JsonFilter("UserInfo")
 @NoArgsConstructor
 @ApiModel(description = "사용자 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
 
     // 현재회원가입 날짜는 미래데이터 사용 X  과거데이터 O
